30cbbaecc788d9275a813c5b46968ed870a095dc
unknown <jean.balazs@bigmoxigames.com>
2024-12-10 17:47:06 -0300

    Added boiler plate code to start implementing config files and adding gameplay abilities to characters through them

---
+++ Source/UE5_MMORPG/Input/DataAssets/InputConfig.h
@@ -0,0 +1,61 @@
+// @Big Moxi Engineers.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "InputConfig.generated.h"
+
+class UInputAction;
+class UObject;
+struct FFrame;
+
+/**
+ * FLyraInputAction
+ *
+ *	Struct used to map a input action to a gameplay input tag.
+ */
+USTRUCT(BlueprintType)
+struct FCustomInputAction
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<const UInputAction> InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+};
+
+/**
+ * UInputConfig
+ *
+ *	Non-mutable data asset that contains input configuration properties.
+ */
+UCLASS(BlueprintType, Const)
+class UE5_MMORPG_API UInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UInputConfig(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category = "Lyra|Pawn")
+	const UInputAction* FindNativeInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = true) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Lyra|Pawn")
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = true) const;
+
+public:
+	// List of input actions used by the owner.  These input actions are mapped to a gameplay tag and must be manually bound.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FCustomInputAction> NativeInputActions;
+
+	// List of input actions used by the owner.  These input actions are mapped to a gameplay tag and are automatically bound to abilities with matching input tags.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FCustomInputAction> AbilityInputActions;
+};
