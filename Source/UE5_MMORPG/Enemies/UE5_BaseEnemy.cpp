83f8f790248da7a6adcf4297a67544b8329c4de2
rafazagolin <rafaelzagolin@outlook.com>
2024-12-16 06:38:34 -0700

    Created base enemy & anim instance classes.

---
+++ Source/UE5_MMORPG/Enemies/UE5_BaseEnemy.cpp
@@ -0,0 +1,35 @@
+﻿$COPYRIGHT_LINE$
+
+
+$MY_HEADER_INCLUDE_DIRECTIVE$
+
+
+// Sets default values
+$PREFIX$$UNPREFIXED_CLASS_NAME$::$PREFIX$$UNPREFIXED_CLASS_NAME$()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void $PREFIX$$UNPREFIXED_CLASS_NAME$::BeginPlay()
+{
+	Super::BeginPlay();
+	$END$
+}
+
+// Called every frame
+void $PREFIX$$UNPREFIXED_CLASS_NAME$::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void $PREFIX$$UNPREFIXED_CLASS_NAME$::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
